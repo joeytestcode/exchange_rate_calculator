a57e2e6ad985485f16db525509ff32809dde1e79
joeyhwangGit <jjoeyhwang@gmail.com>
2022-10-27 20:29:13 +0900

    20221027_2029

--- lib/tool/web_fetcher.dart
+++ lib/tool/web_fetcher.dart
@@ -4,11 +4,14 @@ class WebFetcher {
   static const _maxTry = 4;
 
   static Future<Response> _getResponse(String page) async {
-    return await get(Uri.parse(page), headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': 'true',
-      'Accept': 'application/json'
-    });
+    return await get(
+      Uri.parse(page),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Accept': 'application/json'
+      },
+    );
   }
 
   static Future<Response> getPage(String page) async {
